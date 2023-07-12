dec2861298139a380e8116c1afb997652da61447
Md. Shahriar Kabir Himel <52915408+Himel47@users.noreply.github.com>
2023-07-12 16:24:27 +0600

    Previous HTML code to pdf works

--- jsPDFPractice/tableAttachment.js
+++ jsPDFPractice/tableAttachment.js
@@ -1,14 +1,28 @@
 
-function savePDF() {
-    var element = document.getElementById('mainDiv')
+// function savePDF() {
+//     var element = document.getElementById('mainDiv')
 
-    var doc = new jsPDF()
+//     var doc = new jsPDF()
 
 
-    doc.fromHTML(element, 10, 10, {
-        'witdh': 190
-    }, function () {
-        doc.save('htmlImage.pdf')
-    }
-    )
+//     doc.fromHTML(element, 10, 10, {
+//         'witdh': 190
+//     }, function () {
+//         doc.save('htmlImage.pdf')
+//     }
+//     )
+// }
+
+function processingPDF(){
+
+    html2canvas(document.querySelector('#mainDiv')).then((canvas)=>{
+        let base64image = canvas.toDataURL('details/png')
+        console.log(base64image)
+
+        let pdf = new jsPDF('l', 'px', [1100, 950])
+        pdf.addImage(base64image, 'PNG', 25, 25)
+
+        pdf.save('personalDetails.pdf')
+    })
+
 }
